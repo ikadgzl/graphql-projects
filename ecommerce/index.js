b82b00015f20b3fe62981065a0a473c037a36e9e
ikadgzl <ikadgzl@gmail.com>
2022-03-17 18:37:56 +0300

    added products

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,13 +1,16 @@
 import { ApolloServer, gql } from 'apollo-server';
-import { products } from './data.js';
+import { categories, products } from './data.js';
 
 const typeDefs = gql`
   type Query {
     product(id: ID!): Product
     products: [Product!]!
+    category(id: ID!): Category
+    categories: [Category!]!
   }
 
   type Product {
+    id: ID!
     name: String!
     description: String!
     quantity: Int!
@@ -15,10 +18,16 @@ const typeDefs = gql`
     image: String!
     onSale: Boolean!
   }
+
+  type Category {
+    id: ID!
+    name: String!
+  }
 `;
 
 const resolvers = {
   Query: {
+    products: () => products,
     product: (parent, args, context) => {
       const productId = args.id;
 
@@ -28,7 +37,20 @@ const resolvers = {
 
       return product;
     },
-    products: () => products,
+    categories: () => categories,
+    category: (parent, args, context) => {
+      const categoryId = args.id;
+
+      const category = categories.find(
+        (category) => category.id === categoryId
+      );
+
+      console.log(category);
+
+      if (!category) return null;
+
+      return category;
+    },
   },
 };
 
@@ -37,6 +59,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen().then(({ url }) => {
+server.listen(2132).then(({ url }) => {
   console.log('server is up and running', url);
 });
