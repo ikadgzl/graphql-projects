2ab4410f14ead6be2bbc63d25b63daca866911f5
ikadgzl <ikadgzl@gmail.com>
2022-03-22 06:28:17 +0300

    implemented schema for postgres through prisma and schema for graphql, implemented query and crud mutation for post

--- blog/src/index.ts
+++ blog/src/index.ts
@@ -1,11 +1,26 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema';
-import { Query } from './resolvers';
+import { Query, Mutation } from './resolvers';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export interface Context {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation
+  >;
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
-    Query
+    Query,
+    Mutation
+  },
+  context: {
+    prisma
   }
 });
 
--- blog/src/resolvers/Query.ts
+++ blog/src/resolvers/Query.ts
@@ -1,3 +1,14 @@
+import { Post } from '@prisma/client';
+import { Context } from '..';
+
 export const Query = {
-  hello: () => 'World'
+  posts: async (_, __, { prisma }: Context): Promise<Post[]> => {
+    return await prisma.post.findMany({
+      orderBy: [
+        {
+          createdAt: 'desc'
+        }
+      ]
+    });
+  }
 };
